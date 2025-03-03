faf7bf1c66e6cde883f92711988b6c9cf258bb4a
kinguser-star <jj.pp33@hotmail.com>
2025-03-03 10:28:13 -0500

    commiting after adding all resources till rtassociation

---
+++ output.tf
@@ -0,0 +1,20 @@
+output "my_vpc"{
+value=aws_vpc.main-vpc.id
+}
+
+output "my_subnet"{
+value=aws_subnet.subnet-1.id
+}
+
+output "my_igw"{
+value=aws_internet_gateway.igw.id
+}
+
+output "my_routtable1"{
+value=aws_route_table.my-route-table-1.id
+}
+
+output "my_rtassociation"{
+value=aws_route_table_association.my-route-association.id
+}
+
